95cbd36d0a4c67c3b55baf05b950c4e5e8dff1ee
Román Comelli <roman.comelli@gmail.com>
2020-10-15 15:06:50 -0300

    Modify cmake files to allow compilation as a submodule of Kimera-VIO-ROS.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -6,8 +6,8 @@ include(ExternalProject)
 ExternalProject_Add(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG v1.10.x # Last working version
-    SOURCE_DIR "${CMAKE_BINARY_DIR}/external/googletest-src"
-    BINARY_DIR "${CMAKE_BINARY_DIR}/external/googletest-build"
+    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/external/googletest-src"
+    BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/external/googletest-build"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
